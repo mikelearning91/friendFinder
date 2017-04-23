c1f37d16a8a4abbe200868fcbe280d1e0a8403ce
Michael Wolf <Michael Wolf>
2017-04-22 23:03:34 -0400

    tried another solution to favicon

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ var bodyParser = require("body-parser");
 var path = require("path");
 var formidable = require('formidable');
 var fs = require('fs');
-var favicon = require('serve-favicon');
 var htmlRoutes = require('./routing/html-routes.js');
 var apiRoutes = require('./routing/api-routes.js');
 var fileRoutes = require('./routing/file-routes.js');
@@ -30,9 +29,9 @@ fileRoutes(app);
 apiRoutes(app);
 htmlRoutes(app);
 
-// Uses favicon middleware to serve favicon.ico
+// Serves favicon.ico
 // =============================================================
-app.use(favicon(__dirname + '/public/images/favicon.ico'));
+app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
 
 // Starts the server to begin listening
 // =============================================================
